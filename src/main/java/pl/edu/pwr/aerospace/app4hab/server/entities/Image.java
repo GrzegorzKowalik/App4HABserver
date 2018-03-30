c0dc6afd86b71a7b9f23c8497ee5145cae467bab
JanZmyslowski <37934721+JanZmyslowski@users.noreply.github.com>
2018-03-30 18:20:41 +0200

    Improved JS functions
    Added "test" packgage with "PostsenderImage" and "PostsenderSensor" classes, timestamp for "SensorStatus." class and support for "Photo" and "Sensor" buttons in app4hab.js.

--- src/main/java/pl/edu/pwr/aerospace/app4hab/server/entities/Image.java
+++ src/main/java/pl/edu/pwr/aerospace/app4hab/server/entities/Image.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.Date;
 
 @Entity
 public class Image {
@@ -16,6 +17,7 @@ public class Image {
     private float longitude;
     private float latitude;
     private float altitude;
+
 
     public int getID() {
         return ID;
